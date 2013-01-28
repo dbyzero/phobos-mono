38dfcb41fce3e7633fe0e9beee95344bf41dd087
Leosky <francois.hendrick@gmail.com>
2013-01-28 21:47:40 +0100

    Ajout du support de la Config en fichier INI

---
+++ Phobos/Phobos/Phobos/Engine.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Phobos.Configurations.Keyboard;
+using Phobos.Configurations;
+using System.Configuration;
+using Nini.Config;
+
+namespace Phobos.Engine
+{
+
+    class GameEngine : Microsoft.Xna.Framework.Game
+    {
+
+        #region Fields & Properties
+        GraphicsDeviceManager manager;
+        SpriteBatch spriteBatch;
+        DisplayModeCollection displayModes;
+        #endregion
+
+        #region Constructors
+        public GameEngine(){
+            this.manager = new GraphicsDeviceManager(this);
+            PhobosConfigurationManager.Init("Phobos.ini");
+            PhobosConfigurationManager.SetAutoSave(true);
+            Content.RootDirectory = "assets";
+            
+        }
+        #endregion
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content. Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            #region Initialisation des paramètres graphiques.
+            // TODO: Géré la config externe
+            this.displayModes = GraphicsDevice.Adapter.SupportedDisplayModes;
+            this.Window.AllowUserResizing = false;
+            this.Window.Title = "Phobos";
+            #endregion
+
+            #region Initialisation du clavier
+            string currentLayout = (new WindowsKeyboardExtractor()).getCurrentSystemLayout();
+            if (PhobosConfigurationManager.source.Configs["Keyboard"] == null || PhobosConfigurationManager.source.Configs["Keyboard"].Get("lastKeyboardLayout") == null)
+            {
+                Console.WriteLine("boum");
+                PhobosConfigurationManager.source.AddConfig("Keyboard");
+                PhobosConfigurationManager.set("Keyboard", "lastKeyboardLayout", currentLayout);
+            }
+            else
+            {
+                PhobosConfigurationManager.set("Keyboard", "lastKeyboardLayout", currentLayout);
+            }
+            #endregion
+            // TODO: Add your initialization logic here
+            
+            base.Initialize();
+            
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Add your update logic here
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            // Add your drawing code here
+            base.Draw(gameTime); // Garder en fin de procédure ?
+        }
+
+        /// <summary>
+        /// Cette méthode permet de mettre le jeu en pleine écran tout en conservant l'état de fenêtre (alt tab plus rapides :P);
+        /// </summary>
+        protected void SetWindowedFullScreen()
+        {
+            //On met a jour la résolution d'écran sur la résolution du bureau;
+            DisplayMode screen = this.displayModes.Last<DisplayMode>();
+            this.manager.PreferredBackBufferWidth = screen.Width;
+            this.manager.PreferredBackBufferHeight = screen.Height;
+            this.manager.PreferredBackBufferFormat = screen.Format;
+            this.manager.ApplyChanges(); 
+
+            //on masque récupère la fenêtre.
+            System.Windows.Forms.Form MyGameForm = (System.Windows.Forms.Form)System.Windows.Forms.Form.FromHandle(this.Window.Handle);
+            MyGameForm.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; //Retrait de la bordure
+            MyGameForm.Location = new System.Drawing.Point(0, 0); // on place la fenêtre correctement à l'origine de l'écran.
+        }
+    }
+    
+}
---
+++ Phobos/Phobos/Phobos/Models/World/Generators/BasicGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phobos.Models.World.Generators
+{
+    class BasicGenerator
+    {
+        public static float perlinNoiseHeight(float x, float y, int zoom, int maxHeight, int octaves, float p)
+        {
+            double getnoise = 0;
+            double maxAmplitude = 0;
+            for (int a = 0; a < octaves - 1; a++)//This loops trough the octaves.
+            {
+                //This uses our perlin noise functions. It calculates all our zoom and frequency and amplitude
+                double frequency = Math.Pow(2, a);//This increases the frequency with every loop of the octave.
+                double amplitude = Math.Pow(p, a);//This decreases the amplitude with every loop of the octave.
+                maxAmplitude += amplitude;
+                getnoise += noise(x * frequency / zoom, y / zoom * frequency) * amplitude;
+            }
+
+            return (float)(maxHeight * (getnoise + maxAmplitude) / (maxAmplitude * 2));//Convert to 0-maxHeight values.
+        }
+
+        public static double noise(double x, double y)
+        {
+            double floorx = (double)((int)x);//This is kinda a cheap way to floor a double integer.
+            double floory = (double)((int)y);
+            double s, t, u, v;//Integer declaration
+            s = findnoise2(floorx, floory);
+            t = findnoise2(floorx + 1, floory);
+            u = findnoise2(floorx, floory + 1);//Get the surrounding pixels to calculate the transition.
+            v = findnoise2(floorx + 1, floory + 1);
+            double int1 = interpolate(s, t, x - floorx);//Interpolate between the values.
+            double int2 = interpolate(u, v, x - floorx);//Here we use x-floorx, to get 1st dimension. Don't mind the x-floorx thingie, it's part of the cosine formula.
+            return interpolate(int1, int2, y - floory);//Here we use y-floory, to get the 2nd dimension.
+        }
+
+        public static double findnoise2(double x, double y)
+        {
+            int n = (int)x + (int)y * 57;
+            n = (n << 13) ^ n;
+            int nn = (n * (n * n * 60493 + 19990303) + 1376312589) & 0x7fffffff;
+            return 1.0 - ((double)nn / 1073741824.0);
+        }
+
+	    static public double interpolate(double a,double b,double x)
+	    {
+	        double ft=x * 3.1415927;
+	        double f=(1.0-Math.Cos(ft))* 0.5;
+	        return a*(1.0-f)+b*f;
+
+	    }
+    }
+}
