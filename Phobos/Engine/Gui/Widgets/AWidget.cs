1b7f485863be03b305ce02f439789aa747e025e7
Leosky <kheless@gmail.com>
2013-02-02 00:00:15 +0100

    Uncommitted files...

---
+++ Phobos/Engine/Gui/Widgets/AWidget.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Phobos.Engine;
+
+namespace Phobos.Gui.Widgets {
+    abstract class AWidget : DrawableGameComponent{
+
+        #region Propriétés
+
+        public Point location;
+        public IList<AWidget> children;
+        public AWidget parent;
+
+        #endregion
+
+        #region Constructeurs
+
+        public AWidget(AWidget parent, int x, int y) : base(GameEngine.Instance){
+            this.parent = parent;
+            this.location = new Point(x, y);
+        }
+
+        #endregion
+
+        #region Méthodes publiques
+        #region Transformations
+
+        /// <summary>
+        /// Applique une translation au Widget ainsi qu'à tous ses enfants.
+        /// </summary>
+        /// <param name="dx"></param>
+        /// <param name="dy"></param>
+        public virtual void Translate(int dx, int dy) {
+            this.location.X += dx;
+            this.location.Y += dy;
+
+            foreach (AWidget child in children) {
+                child.Translate(dx, dy);
+            }
+        }
+
+        #endregion
+
+        public abstract override void Draw(GameTime gameTime);
+
+        #endregion
+    }
+}
---
+++ Phobos/Engine/Gui/Widgets/SimpleTextLabel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Phobos.Engine;
+using Phobos.Engine.Content;
+using Microsoft.Xna.Framework.Content;
+
+namespace Phobos.Gui.Widgets
+{
+    class SimpleTextLabel : ALabel
+    {
+
+        #region Propriétés publiques
+
+        public Color color;
+
+        #endregion
+
+        #region Constructeurs
+        public SimpleTextLabel(AWidget parent, int x, int y, string text, string font, Color color)
+            : base(parent, x, y, text, font)
+        {
+            ContentHelper.Load<SpriteFont>( font );
+            this.color = color;
+            this.font = font;
+        }
+        #endregion
+
+        #region Méthodes surchargée
+        public override void Draw(GameTime gameTime) {
+            if( this.parent is SimpleButton && ( this.parent as SimpleButton ).status == SimpleButton.ButtonState.DISABLED ) {
+                GameEngine.spriteBatch.DrawString( ContentHelper.Get<SpriteFont>( font ), text, new Vector2( this.location.X, this.location.Y ), Color.Gray );
+            } else {
+                GameEngine.spriteBatch.DrawString( ContentHelper.Get<SpriteFont>( font ), text, new Vector2( this.location.X, this.location.Y ), Color.White );
+            }
+        }
+
+        #endregion
+        
+    }
+}
