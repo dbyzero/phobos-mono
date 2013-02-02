ad4bb829fd6e23d5f9ef2360b91009d9c2115705
Leosky <kheless@gmail.com>
2013-02-02 20:24:04 +0100

    As usual, uncommited files...

---
+++ Phobos/Engine/Gui/GuiTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Phobos.Engine.Gui.pWidgets;
+
+namespace Phobos.Gui {
+
+    /// <summary>
+    /// Cette classe permet de définir les ressources utilisée par le template de l'ui afin d'alleger le code propre au widgets.
+    /// </summary>
+    class GuiTemplate {
+
+        /// <summary>
+        /// Cette méthode va faire une appel sur la methode statique LoadRessources de chaque widget défini.
+        /// </summary>
+        protected void LoadContent() {
+
+            GuiTemplate.LoadSpecificRessources();
+        }
+
+        /// <summary>
+        /// Cette méthode va appeler explicitement les ressources.
+        /// </summary>
+        public static void LoadSpecificRessources() {
+
+        }
+    }
+}
