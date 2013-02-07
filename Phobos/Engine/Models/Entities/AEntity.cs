950d2fa1628626153bd96416702d24edd8383d6b
dbyzero <cyril@dbyzero.com>
2013-02-07 23:00:17 +0100

    Start to add Entities and Sprite support

---
+++ Phobos/Engine/Models/Entities/AEntity.cs
@@ -0,0 +1,14 @@
+﻿    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+    using Phobos.Engine.Graphics;
+
+    namespace Phobos.Engine.Entities
+    {
+        abstract class AEntity
+        {
+            protected Sprite sprite;
+            protected Vector2 worldPosition;
+            protected Vector2 screenPosition;
+            protected Layer layer;
+        }
+    }
