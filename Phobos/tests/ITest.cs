58b34374e7d22a691c4bf5f7b0313b743a06804f
dbyzero <cyril@dbyzero.com>
2013-07-20 15:34:07 +0200

    Suppression de dependances inutiles dans ITest

--- Phobos/tests/ITest.cs
+++ Phobos/tests/ITest.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Phobos.Engine.View.Proxies.World;
-using Phobos.Engine.View.Proxies.Entities;
-using Microsoft.Xna.Framework.Input;
-using Phobos.Engine.Models.World;
-using Phobos.Models.World.Generators;
-using Phobos.Engine.Models.Light;
-using Phobos.Engine.Controllers.Light;
-using Phobos.Engine.View;
-using Phobos.Engine;
+﻿using Phobos.Engine.View;
 
 namespace Phobos.Test
 {
